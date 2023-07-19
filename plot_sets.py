ffabe9e1ed01aa53831aec42531c9678de10bfa5
AlbertoRoper <alberto.roper.pol@gmail.com>
2023-07-19 16:22:07 +0200

    Update plot_sets.py

--- plot_sets.py
+++ plot_sets.py
@@ -2,7 +2,7 @@
 plot_sets.py is a Python routine that includes some of the settings used in the
 generation of plots and contains functions to generate the plots on the
 different projects of Pencil Code simulations of gravitational waves
-from sources in the early universe (see https://github.com/AlbertoRoper/GW_turbulence).
+from sources in the early universe (see https://github.com/AlbertoRoper/cosmoGW).
 
 Author: Alberto Roper Pol
 Created: 01/01/2021
